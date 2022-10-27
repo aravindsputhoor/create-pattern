689ca87ee4380794987422a98653f240c3d8ced1
Aravind S <aravindsputhoor@gmail.com>
2022-10-27 18:58:51 +0530

    design

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Output Pattern</title>
+</head>
+<body>
+  <div class="text-field" >
+    <input type="text" placeholder="Enter a Keyword">
+    <button>Enter</button>
+    <p>Keyword Count: <span>3</span></p>
+    <table id="">
+
+
+    </table>
+  </div>
+</body>
+</html>
